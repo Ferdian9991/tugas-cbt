929d8ff661a6abcfcf8c73f0e02289d060dc2941
ferdian9991 <ferdisatria15@gmail.com>
2024-07-28 13:12:52 +0700

    add: init project

---
+++ resources/views/components/sidebar.blade.php
@@ -0,0 +1,28 @@
+@auth
+    <div class="main-sidebar sidebar-style-2">
+        <aside id="sidebar-wrapper">
+            <div class="sidebar-brand">
+                <a href="">Menu Utama</a>
+            </div>
+            <div class="sidebar-brand sidebar-brand-sm">
+                <a href="">Menu Utama</a>
+            </div>
+            <ul class="sidebar-menu">
+                <li class="menu-header">Dashboard</li>
+                <li class="{{ Request::is('home') ? 'active' : '' }}">
+                    <a class="nav-link" href="{{ url('home') }}"><i class="fas fa-fire"></i><span>Dashboard</span></a>
+                </li>
+                <!-- profile ganti password -->
+                <li class="menu-header">Profile</li>
+                <li class="{{ Request::is('profile/edit') ? 'active' : '' }}">
+                    <a class="nav-link" href="{{ url('profile/edit') }}"><i class="far fa-user"></i>
+                        <span>Profile</span></a>
+                </li>
+                <li class="{{ Request::is('profile/change-password') ? 'active' : '' }}">
+                    <a class="nav-link" href="{{ url('profile/change-password') }}"><i class="fas fa-key"></i> <span>Ganti
+                            Password</span></a>
+                </li>
+            </ul>
+        </aside>
+    </div>
+@endauth
